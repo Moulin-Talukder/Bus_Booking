44f2964ccb8376c54b9def5e225189cdc2817c69
Moulin Talukder <65370778+Moulin-Talukder@users.noreply.github.com>
2024-07-15 14:37:36 +0600

    bus booking

---
+++ core/app/Models/TicketPriceByStoppage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TicketPriceByStoppage extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $casts = [
+        'source_destination' => 'array'
+    ];
+
+
+    public function ticketPrice(){
+        return $this->belongsTo(TicketPrice::class, 'ticket_price_id');
+    }
+}
